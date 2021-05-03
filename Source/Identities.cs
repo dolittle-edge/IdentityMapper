1468bb1bdc04126a9f155ea2d70ff6b0c9c87ff3
srudaa <sigbjorn.rudaa@raalabs.com>
2021-05-04 00:09:46 +0200

    Restart on change

--- Source/Identities.cs
+++ Source/Identities.cs
@@ -1,10 +1,14 @@
-﻿using RaaLabs.Edge.Modules.Configuration;
+﻿// Copyright (c) RaaLabs. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using RaaLabs.Edge.Modules.Configuration;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace RaaLabs.Edge.IdentityMapper
 {
     [Name("identities.json")]
+    [RestartOnChange]
     public class Identities : ReadOnlyDictionary<string, TimeSeriesByTag>, IConfiguration
     {
         public Identities(IDictionary<string, TimeSeriesByTag> configuration) : base(configuration) { }
