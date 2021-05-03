d80f368422a5d943e969d94abce6fc109572c48d
srudaa <sigbjorn.rudaa@raalabs.com>
2021-05-04 00:10:02 +0200

    Update

--- Source/events/EdgeHubDataPointRemapped.cs
+++ Source/events/EdgeHubDataPointRemapped.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿// Copyright (c) RaaLabs. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics.CodeAnalysis;
 using RaaLabs.Edge.Modules.EdgeHub;
 
 namespace RaaLabs.Edge.IdentityMapper.Events
